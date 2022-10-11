cd793a7bcbbb1834f7379938da79ddae0919f120
DharaneeswaranR <dharaneeshram@gmail.com>
2022-10-11 20:48:42 +0530

    added profile route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import Home from "./routes/Home";
 import Login from "./routes/Login";
 import Register from "./routes/Register";
+import Profile from "./routes/Profile";
 
 export default function App() {
     return (
@@ -10,6 +11,7 @@ export default function App() {
                 <Route exact path='/' element={<Home />} />
                 <Route path='/login' element={<Login />} />
                 <Route path='/register' element={<Register />} />
+                <Route path='/profile' element={<Profile />} />
             </Routes>
         </div>   
     )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,6 @@
+import { UserIcon } from "@heroicons/react/24/outline";
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
     return (
         <nav className="flex justify-between py-3 px-5">
@@ -5,7 +8,10 @@ export default function Navbar() {
                 <h1 className="text-indigo-700 text-3xl font-bold">Tasky</h1>
             </div>
             <div>
-                Profile
+                <UserIcon className="h-6 w-6 m-0 inline-block"/>
+                <Link to='/profile'>
+                    Profile
+                </Link>
             </div>
         </nav>
     )
