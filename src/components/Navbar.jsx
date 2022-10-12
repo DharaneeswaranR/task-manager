9161bc3d9cc61b918e1928988a0560313042fa3a
DharaneeswaranR <dharaneeshram@gmail.com>
2022-10-12 12:57:10 +0530

    added link to home & profile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,24 @@
-import { UserIcon } from "@heroicons/react/24/outline";
-import { Link } from "react-router-dom";
+import { PencilSquareIcon, UserIcon } from "@heroicons/react/24/outline";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Navbar() {
+    const location = useLocation()
+
     return (
-        <nav className="flex justify-between py-3 px-5">
+        <nav className="flex justify-between py-6 px-4 sm:px-6 lg:px-20">
             <div>
                 <h1 className="text-indigo-700 text-3xl font-bold">Tasky</h1>
             </div>
-            <div>
-                <UserIcon className="h-6 w-6 m-0 inline-block"/>
-                <Link to='/profile'>
-                    Profile
+            {location.pathname === '/' ? 
+                <Link className="rounded-lg text-slate-700 text-md px-2 font-semibold transition flex items-center hover:bg-slate-50" to='/profile'>
+                    <UserIcon className="h-5 w-5 text-slate-700 mr-1"/>
+                    <span>Profile</span>
+                </Link> : 
+                <Link className="rounded-lg text-slate-700 text-md px-2 font-semibold transition flex items-center hover:bg-slate-50" to='/'>
+                    <PencilSquareIcon className="h-5 w-5 text-slate-700 mr-1"/>
+                    <span>My Tasks</span>
                 </Link>
-            </div>
+            }
         </nav>
     )
 }
