0a32bd3639365d586aa9e2b8a150dab972a45be7
DharaneeswaranR <dharaneeshram@gmail.com>
2022-10-26 19:18:47 +0530

    added update task option

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,6 +1,6 @@
-import { TrashIcon } from "@heroicons/react/24/outline"
+import { PencilSquareIcon, TrashIcon } from "@heroicons/react/24/outline"
 
-export default function Task({ id, title, completed, description, handleChange, deleteTask }) {
+export default function Task({ id, title, completed, description, handleChange, deleteTask, handleUpdate }) {
     return (
         <div className={`flex w-full my-4 p-4 rounded-2xl ${completed ? 'bg-green-200': 'bg-indigo-50'}`}>
             <div>
@@ -17,9 +17,12 @@ export default function Task({ id, title, completed, description, handleChange, 
                 <label htmlFor={id} className="text-lg font-medium">{title}</label>
                 <p className="text-slate-700 text-md">{description}</p>
             </div>
-            <div>
+            <div className="flex items-start pt-1 space-x-1">
                 <button onClick={() => deleteTask(id)} aria-label='delete'>    
-                    <TrashIcon className="h-5 w-5 ml-auto"/>
+                    <TrashIcon className="h-5 w-5"/>
+                </button>
+                <button onClick={() => handleUpdate(id, title, description)} aria-label='update'>
+                    <PencilSquareIcon className="h-5 w-5" />
                 </button>
             </div>
         </div>
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef, useContext, useMemo } from "react"
-import { PlusIcon, TrashIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { ArrowPathIcon, PlusIcon, XCircleIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import autoAnimate from '@formkit/auto-animate'
 import axios from "axios"
 import { sortTasks } from "../utils/utils"
@@ -10,9 +10,11 @@ import { UserContext } from "../contexts/UserContext"
 
 export default function Home() {
     const [tasks, setTasks] = useState([])
-    const [addTaskField, setAddTaskFiled] = useState(false)
+    const [addTaskField, setAddTaskField] = useState(false)
     const [title, setTitle] = useState("")
     const [description, setDescription] = useState("")
+    const [deadline, setDeadline] = useState(new Date().toISOString().split('T')[0])
+    const [update, setUpdate] = useState("")
     const [user, setUser] = useContext(UserContext)
     const [sort, setSort] = useState(0) // 0: all, 1: completed, 2: Uncompleted
     const listRef = useRef()
@@ -91,6 +93,31 @@ export default function Home() {
         })
     }
 
+    function handleUpdate(id, title, description) {
+        setAddTaskField(true)
+        setTitle(title)
+        setDescription(description)
+        setUpdate(id)
+    }
+
+    async function updateTask() {
+        const updatedTask = await axios.patch(`${process.env.REACT_APP_BACKEND_URL}/task/${update}`, { title, description }, {
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+            }
+        })
+
+        setTasks(prevTasks => {
+            const updatedTasks = prevTasks.map(task => task._id === update ? updatedTask.data : task)
+
+            return updatedTasks
+        })
+
+        setTitle("")
+        setDescription("")
+        setUpdate("")
+    }
+
     async function deleteTask(id) {
         const deletedTask = await axios.delete(`${process.env.REACT_APP_BACKEND_URL}/task/${id}`, {
             headers: {
@@ -110,27 +137,26 @@ export default function Home() {
             <Navbar />
             <div className="w-3/4 lg:w-1/2 mt-10 mx-auto text-slate-800">
                 <h1 className="text-4xl font-semibold">Hello {user.username} 👋</h1>
-                {!addTaskField && 
+                {!addTaskField ? (
                     <button 
                         className="py-1 mt-4 mb-8 pl-3 pr-4 flex items-center rounded-full bg-slate-100 text-md font-semibold hover:bg-slate-200 duration-300"
-                        onClick={() => setAddTaskFiled(true)}
+                        onClick={() => setAddTaskField(true)}
                     >
                         <PlusIcon className="h-6 w-6"/>
                         Add Task
                     </button>
-                }
-                {addTaskField && 
-                    <div className="mb-10 mt-8">
+                ) : ( 
+                    <div className="mb-10 mt-5 bg-indigo-50 p-3 rounded-xl">
                         <div className="flex flex-col w-full">
-                            <div className="flex justify-between">
-                                <h2 className="mb-3 text-md font-semibold">Add Task</h2>
-                                <button aria-label="close" onClick={() => setAddTaskFiled(false)}>
-                                    <XMarkIcon className="h-5 w-5 hover:bg-slate-100 rounded-lg"/>
+                            <div className="flex justify-between mb-3">
+                                <h2 className="text-lg font-semibold">{update ? "Update task" : "Create new task"}</h2>
+                                <button aria-label="close" onClick={() => setAddTaskField(false)}>
+                                    <XMarkIcon className="h-5 w-5 rounded-lg"/>
                                 </button>
                             </div>
                             <input 
                                 type="text" 
-                                className="p-2 w-full bg-indigo-50 mr-3 rounded-md outline-none"
+                                className="p-2 w-full mr-3 rounded-md outline-none"
                                 placeholder="title"
                                 value={title}
                                 onChange={(event) => setTitle(event.target.value)}
@@ -138,34 +164,52 @@ export default function Home() {
                             />
                             <textarea 
                                 type="text" 
-                                className="p-2 mt-3 w-full bg-indigo-50 mr-3 rounded-md outline-none"
+                                className="p-2 mt-3 mb-2 w-full mr-3 rounded-md outline-none"
                                 placeholder="description"
                                 value={description}
                                 onChange={(event) => setDescription(event.target.value)}
                                 onKeyDown={(event) => event.key === "Enter" && addTask()}
                             />
+                            <label htmlFor="date">Deadline</label>
+                            <input 
+                                type="date" 
+                                id="date"
+                                value={deadline}
+                                onChange={(event) => setDeadline(event.target.value)}
+                                className="p-1 w-fit rounded-md"
+                            />
                         </div> 
                         <div className="flex space-x-2">
+                            {!update ? (
+                                <button 
+                                    className="py-1 pl-2 pr-4 mt-4 flex items-center rounded-lg bg-indigo-700 text-white text-sm hover:bg-indigo-800 active:bg-indigo-500 duration-300"
+                                    onClick={addTask}
+                                >
+                                    <PlusIcon className="h-5 w-5 mr-1"/>
+                                    Add
+                                </button> 
+                                ) : (
+                                    <button 
+                                    className="py-1 pl-2 pr-4 mt-4 flex items-center rounded-lg bg-indigo-700 text-white text-sm hover:bg-indigo-800 active:bg-indigo-500 duration-300"
+                                    onClick={updateTask}
+                                >
+                                    <ArrowPathIcon className="h-5 w-5 mr-1"/>
+                                    Update
+                                </button>
+                            )}
                             <button 
-                                className="py-1 pl-2 pr-4 mt-4 flex items-center rounded-lg bg-indigo-700 text-white text-md hover:bg-indigo-800 active:bg-indigo-500 duration-300"
-                                onClick={addTask}
-                            >
-                                <PlusIcon className="h-5 w-5"/>
-                                Add
-                            </button>
-                            <button 
-                                className="py-1 pl-2 pr-4 mt-4 flex items-center rounded-lg bg-slate-100 text-md hover:bg-slate-200 duration-300"
+                                className="py-1 pl-2 pr-4 mt-4 flex items-center rounded-lg bg-indigo-700 text-sm text-white hover:bg-indigo-800 active:bg-indigo-500 duration-300"
                                 onClick={() => {
                                     setTitle("")
                                     setDescription("")
                                 }}
                             >
-                                <TrashIcon className="h-5 w-5"/>
+                                <XCircleIcon className="h-5 w-5 mr-1"/>
                                 Clear
                             </button> 
                         </div>    
                     </div>
-                }
+                )}
                 <div className="flex flex-col mt-4">
                     <h2 className="font-semibold text-xl mb-2">Your tasks 📝</h2>
                     <div className="mb-3">
@@ -196,6 +240,7 @@ export default function Home() {
                                     key={_id}
                                     handleChange={handleChange}
                                     deleteTask={deleteTask}
+                                    handleUpdate={handleUpdate}
                                 />
                             )
                         })}
